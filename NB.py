c909cf3ff166a29fe99b70d3181464e575e11beb
jgeofil <jeremy.geo@gmail.com>
2016-10-12 03:09:18 +0200

    nb

--- NB.py
+++ NB.py
@@ -4,67 +4,78 @@ import pandas as pd
 import sklearn as sk
 import math
 
-
-train = np.array(pd.read_csv('train_in.csv', quotechar='"', skipinitialspace=True))[:4000,:]
-cats = np.array(pd.read_csv('train_out.csv', quotechar='"', skipinitialspace=True))[:4000,:]
-test = np.array(pd.read_csv('train_in.csv', quotechar='"', skipinitialspace=True))[5001:6000,:]
-testC = np.array(pd.read_csv('train_out.csv', quotechar='"', skipinitialspace=True))[5001:6000,:]
-stopwords = nl.corpus.stopwords.words('english')
-cats = cats[:,1]
-testC = testC[:,1]
-tfid = sk.feature_extraction.text.TfidfVectorizer(stop_words='english')
-tfidf_matrix = tfid.fit_transform(train[:,1]).todense()
-
-rawcount = sk.feature_extraction.text.CountVectorizer(stop_words='english')
-rawcount.fit(train[:,1])
-test_matrix =  rawcount.transform(test[:,1]).todense()
-
-#test_matrix =  tfid.transform(test[:,1]).todense()
+SMOOTHING = 1
 
 class NaiveB:
     def __init__(self):
         return
 
     def calculateProbabilities(self):
-        self.possibleOutcomes = np.unique(self.y)
+        self.categories = np.unique(self.y)
         # Discrete data
-        self.kProbabilities = pd.Series()
-        self.colSums = self.X.sum(axis=1)
-        for k in self.possibleOutcomes:
-            self.kProbabilities.loc[k] = self.getProbabilitiesForK(k)
+        self.thetasPerCat = []
+        self.rowSums = self.X.sum(axis=1)
+        for idx, val in enumerate(self.categories):
+            print('Doing category: ' + str(val))
+            self.thetasPerCat.append(self.getThetasForCat(val))
 
-    def getProbabilitiesForK(self, k):
+    def getThetasForCat(self, k):
         featureProbabilities = []
-        for m in range(0,self.m): # For every discrete feature
-            l = self.X.T.A[m]
-            inCat = l[self.y!=k].sum() + 1
-            denom = self.colSums[self.y!=k].sum() + self.m
-            featureProbabilities.append(math.log(inCat/denom))
+        size = len(np.squeeze(self.X[0].A))
+        count = 0
+        for col in self.X.T:
+            count += 1
+            if count%1000 == 0:
+                print(str(count)+' of '+str(size)+' features..')
+            col = np.squeeze(col.A)
+            areNotCatSumForFeature = col[self.y!=k].sum() + SMOOTHING
+            areNotCatSum = self.rowSums[self.y!=k].sum() + len(col) * SMOOTHING
+            featureProbabilities.append(math.log(areNotCatSumForFeature/areNotCatSum))
         return featureProbabilities
 
 
     def train (self, X, y):
-        # Number of features in model
-        self.m = X[0].size
-        print(self.m)
-        # Size of dataset
-        self.n = X.size
+        print('--- TRAINING ---')
         self.X = X
         self.y = y
-
         self.calculateProbabilities()
 
     def predict (self, X):
-
+        print('--- PREDICTING ---')
         out = pd.DataFrame()
-        for x in range(0, len(X.A)):
-            for k in self.possibleOutcomes:
-                out.loc[x,k] = np.multiply(X.A[x], self.kProbabilities[k]).sum()
+        i = 0
+        for col in X:
+            for idx, k in enumerate(self.categories):
+                out.loc[i,k] = np.multiply(np.squeeze(col.A), self.thetasPerCat[idx]).sum()
+            if i % 1000 == 0:
+                print(str(i)+' entries.. ')
+            i += 1
         argmin = out.idxmin(axis=1)
         return argmin
 
 
+inSet = np.array(pd.read_csv('train_in.csv', quotechar='"', skipinitialspace=True, header=0))
+outSet = np.array(pd.read_csv('train_out.csv', quotechar='"', skipinitialspace=True, header=0))
+test = np.array(pd.read_csv('test_in.csv', quotechar='"', skipinitialspace=True, header=0))
+
+size = len(inSet)
+cuttof = 20000
+vallen = 50
+trainSet = inSet[:cuttof,:]
+validateSet = inSet[cuttof+1:cuttof+vallen,:]
+categoriesTrain = outSet[:cuttof,:]
+categoriesValidate = outSet[cuttof+1:cuttof+vallen,:]
+
+tfid = sk.feature_extraction.text.TfidfVectorizer(stop_words='english')
+tfidfTrainSet = tfid.fit_transform(trainSet[:,1])#.todense()
+
+cv = sk.feature_extraction.text.CountVectorizer(stop_words='english').fit(trainSet[:,1])
+#rawCountValidateSet = cv.transform(validateSet[:,1]).todense()
+rawCountValidateSet = cv.transform(test[:,1])#.todense()
+
 nb = NaiveB()
-nb.train(tfidf_matrix, cats)
-arg = nb.predict(test_matrix)
-print((testC == arg).sum())
+nb.train(tfidfTrainSet, categoriesTrain[:,1])
+arg = nb.predict(rawCountValidateSet)
+print(arg)
+arg.to_csv('out.csv')
+#print((categoriesValidate[:,1] == arg).sum()/float(vallen))
